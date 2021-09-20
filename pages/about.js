931e65405229072cf5c580cdb8f6cf6b39a975c5
Aguiluxo91 <apuertadelaguila@gmail.com>
2021-09-20 20:39:11 +0200

    training nextjs

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const About = () => {
+    return ( 
+        <>
+        <Head>
+            <title>Ninja List | About</title>
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ab et sunt ullam. Eveniet obcaecati perferendis esse dolores nemo qui corporis voluptates odit et, nesciunt labore vero possimus, cum reprehenderit.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae minus obcaecati vel earum fugiat! Labore voluptates hic dolor, eaque architecto ullam fugit rerum modi ad, minima ipsa velit sapiente laboriosam.</p>
+        </div>
+        </>
+    )
+}
+ 
+export default About;
